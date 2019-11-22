d390b4c058a9704097a5c8cd42ce9755726d6a15
min.su <min.su@advance.ai>
2019-11-22 16:49:07 +0800

    展示一周的微信步数

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,5 +1,6 @@
 const App = getApp(); //通过getApp方法来引用全局对象
-const db = wx.cloud.database() // 初始化数据库
+const db = wx.cloud.database(); // 初始化数据库
+import wxCharts from "../../utils/wxcharts-min";
 
 /**
  * 用户进入小程序先随机展示气泡和红包，当用户授权后存入 user 表中
@@ -14,9 +15,9 @@ Page({
   },
 
   onLoad: function(options) {
-    this.fetchSetting()
-    this.fetchTips()
-    this.updateRunData()
+    this.fetchSetting();
+    this.fetchTips();
+    this.updateRunData();
   },
 
   getUserInfo(e) {
@@ -26,44 +27,81 @@ Page({
         userInfo: e.detail.userInfo,
       });
       wx.showToast({
-        title: '登录成功',
-        icon: 'none',
-        duration: 1000
-      })
+        title: "登录成功",
+        icon: "none",
+        duration: 1000,
+      });
     }
   },
 
   updateRunData() {
-    const that = this
+    const that = this;
     wx.getWeRunData({
       success(res) {
-        const cloudID = res.cloudID
-        wx.cloud.callFunction({
-          name: 'openapi',
-          data: {
-            action: 'getWeRunAllData',
-            weRunData: wx.cloud.CloudID(cloudID)
-          }
-        }).then(res1 => {
-          that.setData({
-            stepList: res1.result
+        const cloudID = res.cloudID;
+        wx.cloud
+          .callFunction({
+            name: "openapi",
+            data: {
+              action: "getWeRunAllData",
+              weRunData: wx.cloud.CloudID(cloudID),
+            },
           })
-          // 将数据存储在集合中
-        })
+          .then(res1 => {
+            that.setData({
+              stepList: res1.result,
+            });
+            // 将数据存储在集合中
+            that.showCharts(res1.result);
+          });
       },
       fail(err) {
         wx.showToast({
-          title: '提示',
-          content: '未获得步数授权，步数获取失败',
+          title: "提示",
+          content: "未获得步数授权，步数获取失败",
           showCancel: true,
-          cancelText: '知道了',
-          confirmText: '去授权',
+          cancelText: "知道了",
+          confirmText: "去授权",
           success(res) {
-            if (res.confirm) wx.openSetting()
-          }
-        })
-      }
-    })
+            if (res.confirm) wx.openSetting();
+          },
+        });
+      },
+    });
+  },
+  
+
+  showCharts(data) {
+    // 返回步数
+    const arr = data.map(item => item.step)
+    const res =  wx.getSystemInfoSync()
+
+    new wxCharts({
+      canvasId: "lineCanvas",
+      type: "line",
+      categories: ['1', '2', '3', '4', '5', '6', '7'],
+      animation: true,
+      series: [
+        {
+          name: "步数",
+          data: arr.slice(0, 7),
+        },
+      ],
+      xAxis: {
+        disableGrid: true,
+      },
+      yAxis: {
+        min: 0,
+        max: 30000
+      },
+      width: res.windowWidth,
+      height: 180,
+      dataLabel: false,
+      dataPointShape: true,
+      extra: {
+        lineStyle: "curve",
+      },
+    });
   },
 
   /**
@@ -71,17 +109,17 @@ Page({
    * 调用云函数和数据库需要将 this 保存
    */
   fetchTips() {
-    const that = this
-    db.collection('bubble').get({
+    const that = this;
+    db.collection("bubble").get({
       success(res) {
         that.setData({
-          tipList: res.data
-        })
+          tipList: res.data,
+        });
       },
       fail(err) {
-        console.log('fail', err)
-      }
-    })
+        console.log("fail", err);
+      },
+    });
   },
 
   fetchSetting() {
@@ -89,7 +127,7 @@ Page({
       success: res => {
         if (res.authSetting["scope.userInfo"]) {
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          this.fetchOpenId()
+          this.fetchOpenId();
           wx.getUserInfo({
             success: res => {
               App.globalData.userInfo = res.userInfo;
