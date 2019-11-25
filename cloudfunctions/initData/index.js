c58448e2a6548552abf620cb379826cb55af4a3d
sumin <1500160640@qq.com>
2019-11-25 23:28:25 +0800

    解决触发器中循环插入数据到集合中

--- cloudfunctions/initData/index.js
+++ cloudfunctions/initData/index.js
@@ -5,44 +5,58 @@ cloud.init({
 })
 const db = cloud.database()
 const _ = db.command
-const totalTipList = [];
 
 // 云函数入口函数
 exports.main = async(event, context) => {
-  console.log('触发器inti2', event)
   try {
-    return await addTipList()
+    await addBubble()
   } catch (e) {
     console.error(e)
   }
 }
 
-async function addTipList() {
+/**
+ * 先删除已有数据，如果删除返回条数是0 则说明没有删除成功，继续递归调用
+ * 插入时判断当前集合是否为空
+ */
+async function addBubble() {
+  const res = await removeData()
   const temp = await initData()
-  console.log('initData', temp)
-  for (const item of temp) {
-    await db.collection("bubble").add({
-      data: item,
-      success(res) {
-        console.log("success111", res);
-      },
-      fail(err) {
-        console.log("fail222", err);
-      },
-    });
+  const bubbleOld = await getBubbleData()
+  console.log('res', res.stats.removed, bubbleOld.data.length)
+
+  if (!bubbleOld.data.length || res.stats.removed) {
+    console.log('temp', temp.length)
+    for (const item of temp) {
+      await db.collection("bubble").add({
+        data: {
+          ...item,
+          createTime: db.serverDate()
+        }
+      });
+    }
+  } else {
+    console.log('第二次')
+    await addBubble()
   }
+}
+
+async function removeData() {
+  return await db.collection('bubble').where({
+    _id: _.exists(true)
+  }).remove()
 }
 
 /**
  * 初始化气泡数据
  */
 async function initData() {
+  const totalTipList = [];
   const initTipList = await getInitData()
   for (let item of initTipList.data) {
     for (let i = 0; i < item.num; i++) {
       if (i == item.num - 1) delete item.num;
       delete item._id
-      delete item._openid
       totalTipList.push(item);
     }
   }
@@ -51,4 +65,8 @@ async function initData() {
 
 async function getInitData() {
   return await db.collection('initData').get()
+}
+
+async function getBubbleData() {
+  return await db.collection('bubble').get()
 }
