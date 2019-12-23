e565699e6d5138ccd35b045bb8bf233a28c2f00e
min.su <min.su@advance.ai>
2019-12-23 15:39:27 +0800

    update

--- miniprogram/pages/dayRed/dayRed.js
+++ miniprogram/pages/dayRed/dayRed.js
@@ -1,10 +1,12 @@
-// pages/dayRed/dayRed.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
+    everyWeekRed: {
+      countUp: 10,
+      gold: 0,
+      fri_week: 5,
+      refresh: 2
+    },
+    rulesPop: false,
     redList: [
       {
         money: 220,   // 红包金币数量
@@ -65,52 +67,19 @@ Page({
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  showRulesPop(){
+    this.setData({
+      rulesPop: true
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  hidePop(){
+    this.setData({
+      rulesPop: false
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
   onShareAppMessage: function () {
-
+    
   }
 })
